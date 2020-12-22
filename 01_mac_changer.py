ae169a78ec0a4a3b1272bd7a4a025ad62cd67cc3
Forhad Parvez <forhadparvez@outlook.com>
2020-12-22 13:34:54 +0600

    mac changer with arguments

--- 01_mac_changer.py
+++ 01_mac_changer.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-adapterName=input('Adapter Name: ')
-mac=input('MAC: XX:XX:XX:XX:XX:XX ')
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address" )
+    (options, arguments)=parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options
 
-subprocess.call('ifconfig '+adapterName+' down', shell=True)
-subprocess.call('ifconfig '+adapterName+' hw ether '+mac, shell=True)
-subprocess.call('ifconfig '+adapterName+' up', shell=True)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for "+interface+" to "+new_mac)
+    subprocess.call('ifconfig '+interface+' down', shell=True)
+    subprocess.call('ifconfig '+interface+' hw ether '+new_mac, shell=True)
+    subprocess.call('ifconfig '+interface+' up', shell=True)
+
+options=get_arguments()
+change_mac(options.interface, options.new_mac)
